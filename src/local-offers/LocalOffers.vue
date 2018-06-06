b09b9b6079234708a3bbd9bd94946295862ddde0
Stephen Gilboy <sgilboy@meredith.com>
2018-06-06 11:07:20 -0700

    Added a subcomponent to prove that works as well

--- src/local-offers/LocalOffers.vue
+++ src/local-offers/LocalOffers.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h3>Local Offers</h3>
+    <scroll></scroll>
   </div>
 </template>
 
 <script>
+import Scroll from './components/Scroll';
+
 export default {
   name: 'local-offers',
+  components: {
+    'scroll': Scroll,
+  }
 }
 </script>
 
---
+++ src/local-offers/components/Scroll.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="scroller">
+    <marquee>{{msg}}</marquee>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scroll',
+  data() {
+    return {
+      msg: 'IT WORKS!',
+    };
+  },
+}
+</script>
+
+<style scoped>
+  .scroller {
+    width: 100px;
+  }
+</style>
+
